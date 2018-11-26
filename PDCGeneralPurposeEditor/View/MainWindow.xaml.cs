6a34463c7e40ad6788ddab3a7ab1d1ad4b95ba24
shuhey <shuhey.dev@gmail.com>
2018-11-26 17:31:16 +0900

    MainWindow.xamlをViewフォルダに移した。 これからMVVMの構成で行く。極力。

---
+++ PDCGeneralPurposeEditor/View/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace PDCGeneralPurposeEditor.View
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
