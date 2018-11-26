078fee0bb9fde8b319c738d5fa4393c48a06d0d7
shuhey <shuhey.dev@gmail.com>
2018-11-26 17:08:50 +0900

    MaterialDesignとMahAppsを使ってスタイルを整えた。

--- PDCGeneralPurposeEditor/MainWindow.xaml.cs
+++ PDCGeneralPurposeEditor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace PDCGeneralPurposeEditor
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MainWindow_OnDeactivated(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
